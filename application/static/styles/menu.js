83bb8f639069e1870ba6339595a35daa78d7e646
Jorge Rodriguez Chala <jrodri@thoughtworks.com>
2020-04-25 16:54:27 -0500

    Se arregla el menu

--- application/static/styles/menu.js
+++ application/static/styles/menu.js
@@ -14,42 +14,15 @@ function navigateToShoppingCart() {
     window.location.replace("/my_cart");
 }
 
-async function defineVisibility() {
-    return await $.ajax({
-        sync: false,
-        async: true,
-        type: "GET",
-        url: "/roles",
-        dataType: 'json',
-        success: function (data, status, xhttp) {
-            const isAdmin = data['isAdmin']
-            const isLogged = data['isLogged']
-            console.log(isAdmin)
-            console.log(isLogged)
-            if(isAdmin && isAdmin===true){
-                $("#products").show()
-                $("#orders").show()
-            }
-            if (isLogged && isLogged===true){
-                $("#btnSession").html("Cerrar Sesión")
-            }else{
-                $("#btnSession").html("Iniciar Sesión")
-            }
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-            console.log(XMLHttpRequest.responseText)
-        }
-    });
-}
 function isUserLogged() {
     $.ajax({
         type: "GET",
         url: "/manage_session",
         dataType: 'json',
         success: function (data, status, xhttp) {
-            if (data){
+            if (data) {
                 window.location.replace("/");
-            }else{
+            } else {
                 window.location.replace("/login");
             }
         },
