c04050b07f48b9c3156b2b7bb8ec43c98e358278
Jorge Rodriguez Chala <jrodri@thoughtworks.com>
2020-05-27 20:41:34 -0500

    Dinamically add elements to adds product details

--- application/static/styles/create_product.js
+++ application/static/styles/create_product.js
@@ -1,3 +1,5 @@
+let product_detail_index = 0;
+
 function selectFile(id) {
     let files = document.getElementById("file_input").files;
     let file = files[0];
@@ -52,4 +54,24 @@ function uploadFile(file, s3Data, url) {
         }
     };
     xhr.send(postData);
+}
+
+function addProductOptions() {
+    let rootContainer = $("#product-detail-container");
+    const available_units_container = "<div id=\"options_container" + product_detail_index + "\" class=\"row\">\n" +
+        "        <div id=\"available_units_container" + product_detail_index + "\" class=\"block\">\n" +
+        "        <label for=\"available" + product_detail_index + "\">Unidades Disponibles:</label>\n" +
+        "    <input type=\"number\" step=\"1\" pattern=\"\\d+\" min=\"0\" max=\"60000\" value=\"0\" id=\"available" + product_detail_index + "\"\n" +
+        "    name=\"product_available\">\n" +
+        "        </div>\n";
+    const color_container = "<div id=\"color_container" + product_detail_index + "\" class=\"block\">\n" +
+        "        <label for=\"color\">Color:</label>\n" +
+        "    <input type=\"text\" id=\"color" + product_detail_index + "\" name=\"product_color\"/>\n" +
+        "        </div>\n";
+    const size_container = "<div id=\"size_container" + product_detail_index + "\" class=\"block\">\n" +
+        "        <label for=\"size\">Talla:</label>\n" +
+        "    <input type=\"text\" id=\"size" + product_detail_index + "\" name=\"product_size\"/>\n" +
+        "        </div>\n" +
+        "        </div>\n";
+    rootContainer.append(available_units_container + color_container + size_container);
 }
