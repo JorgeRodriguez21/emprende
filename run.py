97439eaa6e524f7e3d8041ee6dc6101f1ac35746
Jorge Rodriguez Chala <jrodri@thoughtworks.com>
2020-06-30 10:24:52 -0500

    Logger para errores al crear productos

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
-import os
+import logging
 
 from app import config_app
 
 app = config_app()
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
